c37bb669940df74031081ba591e00c8ed0e1bc27
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-07-22 20:05:01 +0300

    Update BicycleDemo.java

--- Bicycle/BicycleDemo.java
+++ Bicycle/BicycleDemo.java
@@ -9,25 +9,25 @@ class BicycleDemo {
 
     public static void main(String[] args) {
 
-        // Create two different 
+        // Create two different
         // Bicycle objects
-        Bicycle bike1 = new Bicycle();
-        Bicycle bike2 = new Bicycle(5, 40, 2);
+        Bicycle roadBike1 = new RoadBicycle();
+        Bicycle roadBike2 = new RoadBicycle(5, 40, 2);
 
-        // Invoke methods on 
+        // Invoke methods on
         // those objects
-        bike1.changeCadence(50);
-        bike1.speedUp(10);
-        bike1.changeGear(2);
-        bike1.printStates();
+        roadBike1.changeCadence(50);
+        roadBike1.speedUp(10);
+        roadBike1.changeGear(2);
+        roadBike1.printStates();
 
-        bike2.changeCadence(50);
-        bike2.speedUp(10);
-        bike2.changeGear(2);
-        bike2.changeCadence(40);
-        bike2.speedUp(10);
-        bike2.changeGear(3);
-        bike2.printStates();
+        roadBike2.changeCadence(50);
+        roadBike2.speedUp(10);
+        roadBike2.changeGear(2);
+        roadBike2.changeCadence(40);
+        roadBike2.speedUp(10);
+        roadBike2.changeGear(3);
+        roadBike2.printStates();
 
     }
 }
